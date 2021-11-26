22ee6451e24892461bf3c4e0a94c6d049f701d4d
Gustavo Tremont <gustavotremontsr@gmail.com>
2021-11-26 22:02:27 +0100

    add styles and script for the dropdown menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-// ADD JS AND CHANGE NAME
+document.getElementById('hamburger').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('myMenu').style.height = '100%';
+});
+
+document.getElementById('buttonX').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('myMenu').style.height = '0%';
+});
