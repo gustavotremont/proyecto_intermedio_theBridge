cc32101da680deef92ea81daf3442fd6365ee890
Gustavo Tremont <gustavotremontsr@gmail.com>
2021-11-29 00:08:14 +0100

    add .env variables

--- utils/dbPostgres.js
+++ utils/dbPostgres.js
@@ -1,17 +1,10 @@
 const { Pool } = require("pg");
 
-// const pool = new Pool({
-//     user: process.env.USER,
-//     host: process.env.HOST,
-//     database: process.env.DATABASE,
-//     password: process.env.PASSWORD,
-// });
-
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "intermedio_users",
-    password: "0000",
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
 });
 
 module.exports = pool;
