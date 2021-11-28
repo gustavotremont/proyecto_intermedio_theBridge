6951bb426e800827eca82be9e403ecffa8f87de2
Gustavo Tremont <gustavotremontsr@gmail.com>
2021-11-29 00:01:41 +0100

    refactor controllers

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -1,24 +1,43 @@
-const dataFavorite = require("../models/favorites");
+const Favorite = require("../models/favorites");
 
-const getFavorites = async (req, res) => {
-    const data = await dataFavorite.getAllFavoritesByUser(req.params.email);
-    res.status(200).json({ favorites: data });
+const getFavorite = async (req, res) => {
+    try {
+        if(req.query.currectFavoriteId){
+            const result = await Favorite.getFavorite(req.query.currectFavoriteId);
+            res.status(200).json(result);
+        }else if(req.query.currectUserEmail) {
+            const result = await Favorite.getAllFavoritesByUser(req.query.currectUserEmail);
+            res.status(200).json({ favorites: result });
+        }else{
+            res.status(400).json({"error":err})
+        }   
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
+    
+    
 };
 
 const createFavorite = async (req, res) => {
-    const result = await dataFavorite.createFavorite(req.body);
-    console.log(result);
-    res.status(201).send("nueva oferta añadida a favoritos");
+    try {
+        const result = await Favorite.createFavorite(req.body);
+        res.status(201).send("nueva oferta añadida a favoritos");        
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
 };
 
 const deleteFavorite = async (req, res) => {
-    const result = await dataFavorite.deleteFavorite(req.body.id);
-    console.log(result);
-    res.status(200).send("oferta eliminada de favoritos");
+    try {
+        const result = await Favorite.deleteFavorite(req.body.id);
+        res.status(200).send("oferta eliminada de favoritos");
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
 };
 
 const favorites = {
-    getFavorites,
+    getFavorite,
     createFavorite,
     deleteFavorite
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,55 @@
-const dataUser = require("../models/users");
+const User = require("../models/users");
 
-const getUserByEmail = async (req, res) => {
-    const data = await dataUser.getUser(req.params.email);
-    res.status(200).json(data);
+const createUser = async (req, res) => {
+    try {
+        const result = await User.createUser(req.body);
+        res.status(201).send("nuevo usuario creado");        
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }    
+};
+
+const getUser = async (req, res) => {
+    try {
+        if (req.params.email) {
+            const result = await User.getUser(req.params.email);
+            res.status(200).json(result);
+        } else {
+            const result = await User.getAllUsers();
+            res.status(200).json({ users: result });
+        }        
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {        
+        const result = await User.deleteUser(req.body.email, req.body.password);
+        res.status(200).send("usuario borrado exitosamente");
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {        
+        const result = await User.updateUser(
+            req.body.email,
+            req.body.password,
+            req.body.newPassword
+        );
+        res.status(200).send("La clave del ususario a sido cambiada");
+    } catch (err) {
+        res.status(400).json({"error":err})
+    }
 };
 
 const users = {
-    getUserByEmail
+    createUser,
+    getUser,
+    deleteUser,
+    updateUser,
 };
 
 module.exports = users;
