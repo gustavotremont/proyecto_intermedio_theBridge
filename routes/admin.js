067884b25ddbe4ad4856e3fa58a8287d5a8f6bcd
Gustavo Tremont <gustavotremontsr@gmail.com>
2021-12-09 23:27:48 +0100

    create middleware for errors

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,9 @@ const routes = express.Router();
 const user = require('../controllers/user')
 const offer = require("../controllers/offer");
 
-routes.get("/user/:action?", passport.authenticate('jwt', {session: false}), user.getUser); //Mostrar Lista de Usuarios
-routes.get('/dashboard/:action?', passport.authenticate('jwt', {session: false}), offer.getOffers)
+const isAdmin = require('../middlewares/isAdmin')
 
+routes.get("/user/:action?", passport.authenticate('jwt', {session: false}), isAdmin, user.getUser); //Mostrar Lista de Usuarios
+routes.get('/dashboard/:action?', passport.authenticate('jwt', {session: false}), isAdmin, offer.getOffers)
 
 module.exports = routes;
